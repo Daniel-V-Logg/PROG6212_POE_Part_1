083a2cfe88690ed63e81d4d37503b228e87dc60b
Daniel-V-Logg <ST10310998@vcconnect.edu.za>
2024-09-10 22:22:59 +0200

    Add README.md.

---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contract_Monthly_ClaimSystem__CMCS_.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        public IActionResult Submit()
+        {
+            return View();
+        }
+
+        public IActionResult Approve()
+        {
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Contract Monthly ClaimSystem (CMCS)
